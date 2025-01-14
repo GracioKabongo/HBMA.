5568b7697e1ccc90d3cf696388e02c5a32e40f8f
Gracio Kabongo Lumbala <graciolumbala01@gmail.com>
2025-01-14 18:34:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    setTimeout(function () {
+        document.getElementById("preloader").style.display = "none"; 
+        document.getElementById("content").style.display = "block"; 
+    }, 1500); 
+};
+   
